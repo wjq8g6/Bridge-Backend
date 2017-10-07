717722bfc8b34e2017b896b2366bcb9b0e21f315
Jiaqi (Alex) Wang <jwang98@stanford.edu>
2017-10-07 16:25:23 -0700

    Fixed app.py

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask
+from flask import Flask, json
 import pandas as pd
 import numpy as np
+from firebase import firebase
 app = Flask(__name__)
+
+@app.route("/init")
+def init():
+    global fb
+    aut = firebase.FirebaseAuthentication('hY5kvBhsK4MZbShXIxeMEo0rOWyfL7LdW5TC95Od', 'wjq8g6@gmail.com')
+    fb = firebase.FirebaseApplication('https://bridge-fb5ab.firebaseio.com', aut)
+    global data
+    data = fb.get("", None)
+    return "Finished Initialization"
 
 @app.route("/name/<int:name_id>")
 def get_name(name_id):
-    data = pd.read_csv('data.csv')
     return data['Name'][name_id]
 
 @app.route("/info/<int:id>")
 def returnIDs(id):
-    data = pd.read_csv('data.csv')
-    stu = data.iloc[id].values
-    ret = ""
-    for val in stu:
-        ret += str(val) + ","
-    return ret
+    return data[id]
 
-
+'''
 @app.route("/adduser/<string:info>")
 def addStu(info):
     data = pd.read_csv('data.csv')
@@ -26,18 +30,39 @@ def addStu(info):
     data = data.append(df_new)
     data.to_csv("data.csv",index = False)
     return "Success"
+'''
 
 
-@app.route("/getnn/<int:id>/req/<string:class_name>")
-def getNN(id, class_name):
-    data = pd.read_csv('data.csv')
-    candidates = data.loc[data[class_name] == 1]
-    ind_list = candidates.index
-    pot = candidates.drop('Name', axis=1).values
-    data = data.drop('Name', axis=1)
-    stu = data.iloc[id].values
-    lst = []
-    for row in pot:
-        lst.append(np.linalg.norm(stu - row))
-    ind = lst.index(min(lst))
-    return returnIDs(ind_list[ind])
+@app.route("/getnn/<int:id>/<string:class_name>/<int:num_ret>")
+def getNN(id, class_name, num_ret):
+    stuids = []
+    dists = []
+    target = data[id]
+    for i in range(len(data)):
+        if i != id:
+            stu = data[i]
+            courses = stu['Courses']
+            if class_name in courses:
+                stuids.append(i)
+                dists.append(calDist(stu,target))
+    if num_ret > len(dists):
+        return str(stuids)
+    else:
+        ind = np.argpartition(dists, num_ret)[:num_ret]
+        ret = ''
+        for k in ind:
+            ret += str(stuids[k])+','
+        ret = ret[:-1]
+        return ret
+
+
+def calDist(stu, target):
+    headers = ['Year','Gender','Ethnicity','Major']
+    dist = 0
+    for head in headers:
+        if stu[head] != target[head]:
+            dist += 1
+    s1 = set(stu['Courses'])
+    s2 = set(target['Courses'])
+    dist += 16 - len(s1.intersection(s2))
+    return dist
