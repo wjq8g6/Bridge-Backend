7b16204ef5de888cd0b76fad4f0a47cc28f4505b
Jiaqi (Alex) Wang <jwang98@stanford.edu>
2017-10-07 20:25:04 -0700

    Final

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def getNN(id, class_name, num_ret):
         if i != id:
             stu = data[i]
             courses = stu['Courses']
-            if class_name in courses and int(stu['Year']) < int(target['Year']):
+            if class_name in courses:
                 stuids.append(i)
                 dists.append(calDist(stu,target))
     if num_ret > len(dists):
